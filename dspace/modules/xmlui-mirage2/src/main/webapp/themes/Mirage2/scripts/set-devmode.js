169815c05aea04be9aebfbe14f7bf4a471053a42
root <root@master71.npi.utn>
2015-02-13 13:49:28 -0300

    RIA FUNCIONANDO con DSPACE 5

--- dspace/modules/xmlui-mirage2/src/main/webapp/themes/Mirage2/scripts/set-devmode.js
+++ dspace/modules/xmlui-mirage2/src/main/webapp/themes/Mirage2/scripts/set-devmode.js
@@ -1,15 +1,11 @@
-/**
+/*
  * The contents of this file are subject to the license and copyright
  * detailed in the LICENSE and NOTICE files at the root of the source
  * tree and available online at
  *
  * http://www.dspace.org/license/
  */
-
-/**
- * You can put your own style in this file.
- * It will be included in all color schemes
- */
-
-@import "UTN-styles/flatty";
-@import "UTN-styles/style";
+(function(){
+    window['DSpace'] = window['DSpace'] || {};
+    window.DSpace['dev_mode'] = true;
+}).call(this);
